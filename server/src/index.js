e96ca63cdb1bd6a32c0f6159efce50bd8f4988d4
Ansh <ansh67893@mail.com>
2024-09-24 20:13:49 +0530

    m

--- server/src/index.js
+++ server/src/index.js
@@ -22,11 +22,14 @@ app.use(cors({
   }));
 
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://task-m-frontend-bay.vercel.app"); // Update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://task-m-frontend-bay.vercel.app");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
-  });
+  })
+
+
   
 
 const options = {
